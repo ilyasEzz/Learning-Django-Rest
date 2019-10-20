8051a733e7702e50e54dd0f11cc5930b63f832a2
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-10-21 00:42:20 +0700

    custom user profile

---
+++ profiles_api/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+
+    def create_user(self, login, name, password=None):
+        if not login:
+            raise ValueError('user must have a login')
+
+        user = self.model(login=login, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, login, name, password):
+        user = self.create_user(login, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    login = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'login'
+
+    def get_full_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.login
