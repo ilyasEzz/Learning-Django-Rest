f6d0a69c440900b19a1bff2f23c3a537cb1a2a8a
Ilyas Ezzahid <ezzil1997@gmail.com>
2019-10-22 02:06:39 +0700

    Added HelloViewSet

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
 
-from .views import HelloApiView
+router = DefaultRouter()
+router.register('hello-viewset',  views.HelloViewSet,
+                base_name='hello-viewset')
+
 
 urlpatterns = [
-    path('hello-api/', HelloApiView.as_view()),
+    path('', include(router.urls)),
+    # path('hello-api/', HelloApiView.as_view()),
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,20 +1,105 @@
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-# Create your views here.
+
+from .serializers import HelloSerializer
 
 
-class HelloApiView(APIView):
+# class HelloApiView(APIView):
+#     """ Test APIView """
 
-    def get(self, request, format=None):
-        an_apiview = [
-            'Uses HTTP methods as function',
-            'Is similar to a tradditional django view',
-            'is mapped manually to URLS'
+#     serializer_class = HelloSerializer
+
+#     def get(self, request, format=None):
+#         """Returns a list of APIView Features"""
+
+#         an_apiview = [
+#             'Uses HTTP methods as function',
+#             'Is similar to a tradditional django view',
+#             'is mapped manually to URLS'
+#         ]
+
+#         context = {
+#             'message': 'Hello Universe',
+#             'an_apiview': an_apiview
+#         }
+
+#         return Response(context)
+
+#     def post(self, request):
+#         """ Create a hello message with our name """
+
+#         serializer = self.serializer_class(data=request.data)
+
+#         if serializer.is_valid():
+#             name = serializer.validated_data.get('name')
+#             message = f'Hello {name}'
+#             return Response({'message': message})
+#         else:
+#             return Response(
+#                 serializer.errors,
+#                 status=status.HTTP_400_BAD_REQUEST
+#             )
+
+#     def put(self, request, pk=None):
+#         """ Update an object """
+#         return Response({'method': 'PUT'})
+
+#     def patch(self, request, pk=None):
+#         """ Update partially an object """
+#         return Response({'method': 'PATCH'})
+
+#     def delete(self, request, pk=None):
+#         """ Delete an object """
+#         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(ViewSet):
+    """ Test Api View Set """
+
+    serializer_class = HelloSerializer
+
+    def list(self, request):
+        api_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automaticly maps to URLS, USING Routers',
+            'add more functionnality for less code'
         ]
 
         context = {
             'message': 'Hello Universe',
-            'an_apiview': an_apiview
+            'api_viewset': api_viewset
         }
 
         return Response(context)
+
+    def create(self, request):
+        """ Create a new hello message """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """ Getting an object by his primary key """
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """ Updating an object by his primary key """
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ Upddating partially an object by his primary key """
+        return Response({'http_method': 'POST'})
+
+    def destroy(self, request, pk=None):
+        """ Removing an object by his primary key """
+        return Response({'http_method': 'DELETE'})
